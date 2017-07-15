50ed0294a6d7da367c7dd65c27b112728735cfb8
uboat46 <uboat46@hotmail.com>
2017-07-14 20:33:34 -0500

    lols

---
+++ cabras/public/app/routes.js
@@ -0,0 +1,21 @@
+(function(){
+    'use strict';
+    var app = angular.module('app');
+    
+    app.config(function($routeProvider) {
+        $routeProvider
+        .when("/", {
+            templateUrl : "app/components/user.html",
+            controller: "userCtrl"
+        })
+        .when("/red", {
+            templateUrl : "red.htm"
+        })
+        .when("/green", {
+            templateUrl : "green.htm"
+        })
+        .otherwise( {
+            redirectTo:'/'
+        });
+    });
+})();
