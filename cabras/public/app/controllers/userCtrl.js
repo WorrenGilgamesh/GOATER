326121e8b8390ccfdeac515ca1741ae9c3424afb
uboat46 <uboat46@hotmail.com>
2017-07-14 20:38:47 -0500

    user

--- cabras/public/app/controllers/userCtrl.js
+++ cabras/public/app/controllers/userCtrl.js
@@ -2,7 +2,14 @@
     'use strict';
 
     var app = angular.module('app');
-    app.controller('userCtrl',function($scope){
-        
+    app.controller('userCtrl',function($scope,$http){
+        $http({
+        method : "GET",
+        url : "../public/Usuario"
+    }).then(function mySuccess(response) {
+        console.log(response.data);
+    }, function myError(response) {
+        $scope.myWelcome = response.statusText;
+    });
     });
 })();
